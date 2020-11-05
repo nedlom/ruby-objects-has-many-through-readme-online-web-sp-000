d269166c3664450acd49dd7dee7f5591c67cd802
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 16:31:24 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,33 +1,59 @@
+require 'pry'
 class Waiter
-  
-  attr_accessor :name, :yrs_experience
   
   @@all = []
   
-  def initialize(name, yrs_experience)
-    @name = name
-    @yrs_experience = yrs_experience
-    @@all << self
+  def initialize(name, yrs_exp)
+    self.class.all << self
   end
   
   def self.all
     @@all
   end
   
-  def new_meal(customer, total, tip=0)
+  def new_meal(customer, total, tip)
     Meal.new(self, customer, total, tip)
   end
   
   def meals
-    Meal.all.select do |meal|
-      meal.waiter == self
-    end
+    Meal.all.select {|m| m.waiter == self}
   end
   
   def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b.tip
-    end
-    best_tipped_meal.customer
+    meals.sort {|a, b| a.tip <=> b.tip}.last.customer
   end
 end
+
+# class Waiter
+  
+#   attr_accessor :name, :yrs_experience
+  
+#   @@all = []
+  
+#   def initialize(name, yrs_experience)
+#     @name = name
+#     @yrs_experience = yrs_experience
+#     @@all << self
+#   end
+  
+#   def self.all
+#     @@all
+#   end
+  
+#   def new_meal(customer, total, tip=0)
+#     Meal.new(self, customer, total, tip)
+#   end
+  
+#   def meals
+#     Meal.all.select do |meal|
+#       meal.waiter == self
+#     end
+#   end
+  
+#   def best_tipper
+#     best_tipped_meal = meals.max do |meal_a, meal_b|
+#       meal_a.tip <=> meal_b.tip
+#     end
+#     best_tipped_meal.customer
+#   end
+# end
