43b50d7ee424c32c08c384dcb909ca7785b7647e
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-05 18:01:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,4 +1,17 @@
-
+class Waiter
+  
+  attr_accessor :name, :years
+  
+  def initialize(name, years)
+    @name =  name 
+    @years = years
+  end
+  
+  def self.all
+    @@all = []
+  end
+  
+end
 
 
 
