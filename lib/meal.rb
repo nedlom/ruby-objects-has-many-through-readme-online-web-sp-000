fd435938868dcaa039e856efa3e2d3ef060fa32b
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-05 18:00:37 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,4 +1,18 @@
 class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
+  
+  def self.all
+    @@all = []
+  end
+  
 end
 
 
